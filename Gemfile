ebc4d11f9a5d0895612fc17e72e7b376f5ef4f61
Fyodor Tsvetkov <fyodor.tsvetkov@gmail.com>
2013-06-28 01:19:20 +0300

    Add sinatra-assetpack gem support, replace tabs with spaces

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source "https://rubygems.org"
 ruby '1.9.3'
 
 gem 'sinatra', '1.4.3'
+gem 'sinatra-assetpack', :require => 'sinatra/assetpack'
--- app.rb
+++ app.rb
@@ -3,90 +3,115 @@
 #Sinatra Guestbook
 
 require 'sinatra'
+require 'sinatra/assetpack'
+
+set :root, File.dirname(__FILE__)
+
+configure do
+  assets do
+    serve '/js',     from: 'public/bootstrap/js'
+    serve '/css',    from: 'public/bootstrap/css'
+    serve '/img',    from: 'public/bootstrap/img'
+
+    js :bootstrap, [
+      '/js/bootstrap.js'
+    ]
+
+    css :bootstrap, [
+      '/css/bootstrap.css',
+      '/css/bootstrap-responsive.css'
+    ]
+
+    js_compression  :jsmin
+    css_compression :simple
+  end
+end
 
 get '/' do
-	erb:index
+  erb:index
 end
 
 get '/clear' do
-	clear_file
+  clear_file
+
+  redirect to('/')
 end
 
 post '/' do
-	author = params[:author].strip.chomp
-	message = params[:message].strip.chomp
+  author = params[:author].strip.chomp
+  message = params[:message].strip.chomp
 
-	if author != nil && author.empty? != true && 
-		message != nil && message.empty? != true
-		save_post(author, message)
-	end
+  if author != nil && author.empty? != true && 
+    message != nil && message.empty? != true
+    save_post(author, message)
+  end
 
   erb:index
 end
 
 def render_posts
-	posts = get_posts
+  posts = get_posts
 
-	html_posts = ""
+  html_posts = ""
 
-	if posts.length > 0
-		posts.each do |post|
-			html_posts << "<div><small><strong>#{post[:name]}</strong> on #{post[:date]} wrote</small><blockquote>#{render_message(post[:message])}</blockquote></div>"
-		end
-	else
-		html_posts = "<div>Guestbook is empty... =(</div>"
-	end
+  if posts.length > 0
+    posts.each do |post|
+      html_posts << "<div><small><strong>#{post[:name]}</strong> on #{post[:date]} wrote</small><blockquote>#{render_message(post[:message])}</blockquote></div>"
+    end
+  else
+    html_posts = "<div>Guestbook is empty... =(</div>"
+  end
 
-	html_posts
+  html_posts
 end
 
 def render_message(message)
-	paragraphs = ""
+  paragraphs = ""
 
-	message.each_line('\n') do |line|
-		if line.empty? != true
-			paragraphs << "#{line.chomp('\n')}<br />"
-		end
-	end
+  message.each_line('\n') do |line|
+    if line.empty? != true
+      paragraphs << "#{line.chomp('\n')}<br />"
+    end
+  end
 
-	paragraphs.chomp('<br />')
+  paragraphs.chomp('<br />')
 end
 
 def get_posts
-	file_content = File.open('posts.txt'){ |file| file.read }
-	unparsed_posts = file_content.split("=====\n")
+  file_content = File.open('posts.txt'){ |file| file.read }
+  unparsed_posts = file_content.split("=====\n")
 
-	pattern = /^date:\s(.+)\nname:\s(.+)\nmessage:\s(.+)$/
+  pattern = /^date:\s(.+)\nname:\s(.+)\nmessage:\s(.+)$/
 
-	posts ||= []
+  posts ||= []
 
-	unparsed_posts.each do |post|
-		pattern =~ post.gsub(/\r\n/, '\n')
+  unparsed_posts.each do |post|
+    pattern =~ post.gsub(/\r\n/, '\n')
 
-		parse_data = Regexp.last_match
+    parse_data = Regexp.last_match
 
-		parsed_post = Hash.new
-		parsed_post[:date] = parse_data[1].strip.chomp
-		parsed_post[:name] = parse_data[2].strip.chomp
-		parsed_post[:message] = parse_data[3].strip.chomp
+    parsed_post = Hash.new
+    parsed_post[:date] = parse_data[1].strip.chomp
+    parsed_post[:name] = parse_data[2].strip.chomp
+    parsed_post[:message] = parse_data[3].strip.chomp
 
-		posts << parsed_post
-	end
+    posts << parsed_post
+  end
 
-	posts
+  posts
 end
 
-def save_post (author, message)	
-	File.open('posts.txt', 'a') do |file| 
-		file.puts "date: #{Time.new}"
-		file.puts "name: #{author}"
-		file.puts "message: #{message}"
-		file.puts "====="
-	end
+def save_post (author, message) 
+  File.open('posts.txt', 'a') do |file| 
+    file.puts "date: #{Time.new}"
+    file.puts "name: #{author}"
+    file.puts "message: #{message}"
+    file.puts "====="
+  end
 end
 
 def clear_file
-	File.open('posts.txt', 'w') do |file| 
-		file.print ''
-	end
+  File.open('posts.txt', 'w') do |file| 
+    file.print ''
+  end
 end
